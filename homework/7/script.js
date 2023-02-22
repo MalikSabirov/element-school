88c45711cca3b040db9a5543991103db7a59d2df
Malik Sabirov <maaliksabirov@gmail.com>
2023-02-22 21:42:15 +0600

    feat: collect comments in posts

--- homework/7/script.js
+++ homework/7/script.js
@@ -2,6 +2,7 @@ const URL_POSTS = "https://jsonplaceholder.typicode.com/posts";
 const URL_COMMENTS = "https://jsonplaceholder.typicode.com/comments";
 
 let posts;
+let comments;
 
 const getData = (url) => {
   return fetch(url)
@@ -9,30 +10,39 @@ const getData = (url) => {
     .catch((e) => console.error(e));
 };
 
-getData(URL_POSTS).then((res) => {
-  console.log(res);
-  // posts = toHashMap(res);
-});
-
 const toHashMap = (arr) => {
   const obj = {};
 
-  arr.reduce();
   arr.map((item) => (obj[item.id] = item));
 
   return obj;
 };
 
-Promise.allSettled([getData(URL_POSTS), getData(URL_COMMENTS)]).then(
-  (responses) => {
+Promise.allSettled([getData(URL_POSTS), getData(URL_COMMENTS)])
+  .then((responses) => {
     responses.map((result) => {
-      console.log(result);
-
-      if (result.value[0].userId) {
-        console.log("posts: ", result);
-      } else {
-        console.log("comments: ", result);
+      if (result.status === "fulfilled") {
+        if (result.value[0].userId) {
+          posts = toHashMap(result.value);
+        } else {
+          comments = result.value;
+          console.log(comments);
+        }
       }
     });
-  }
-);
+  })
+  .then(() => {
+    comments.map((comment) => {
+      const post = posts[comment.postId];
+
+      if (post.children) {
+        post.children.push(comment);
+      } else {
+        post.children = [comment];
+      }
+    });
+  })
+  .then(() => {
+    console.log(posts);
+  })
+  .catch(console.error);
